36c1545189a70e8b70344240a3926414ee4b6aae
cek0060 <cek0060@auburn.edu>
2022-12-01 11:38:17 -0600

    β^ function

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -1,10 +1,18 @@
-# Functions given by equations in assignment - will be used in package functions later
 
-initial_beta <- function(X, y) {
-  return (solve(t(X) %*% X) %*% t(X) %*% y)
-  }
+
 
 log_regression <- function(X, y, beta) {
   p <- 1 / (1 + exp(-X %*% beta))
   return(sum(-((1 - y) * log(1 - p)) - (y * log(p))))
-  }
+}
+
+initial_beta_least_squares <- function(X, y) {
+  return (solve(t(X) %*% X) %*% t(X) %*% y)
+}
+
+
+#function for generating β^ - vector of logistic regression coefficients
+beta_hat <- function(predictor, response) {
+  return (optim(get_initial_beta(predictor, response), log_regression, X = predictor, y = response)$par)
+}
+  
