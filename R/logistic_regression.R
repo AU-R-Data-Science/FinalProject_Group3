2800cb90ec2b96689a7db2e448ad9d29eddc4a42
cek0060 <cek0060@auburn.edu>
2022-12-01 10:46:08 -0600

    Initial Logistic Regression functions

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -1,1 +1,10 @@
+# Functions given by equations in assignment - will be used in package functions later
 
+initial_beta <- function(X, y) {
+  return (solve(t(X) %*% X) %*% t(X) %*% y)
+  }
+
+log_regression <- function(X, y, beta) {
+  p <- 1 / (1 + exp(-X %*% beta))
+  return(sum(-((1 - y) * log(1 - p)) - (y * log(p))))
+  }
