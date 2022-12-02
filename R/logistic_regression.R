a9aab996376cbab51e24c4d7a2f09b7c22175896
cek0060 <cek0060@auburn.edu>
2022-12-02 10:46:10 -0600

    Confusion matrix - roxygen2 description

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -12,6 +12,30 @@ beta_hat <- function(predictor, response) {
   return (optim(get_initial_beta(predictor, response), log_regression, X = predictor, y = response)$par)
 }
 
+#' Confusion Matrix and Metrics
+#' 
+#' @description 
+#' This function performs logistic regression and then computes a confusion matrix and some common metrics from the confusion matrix.
+#' 
+#' @param predictor A matrix containing values of the predictors
+#' @param response A vector containing values of the responses
+#' @param cutoff A double value - predictions greater than the cutoff are set to one, predictions less than the cutoff are set to zero.
+#' 
+#' @return A list containing the following values:
+#' \describe{
+#' \item{tp}{The double value of true postives. Actual = predicted = 1.}
+#' \item{tn}{The double value of true negatives. Actual = predicted = 0.}
+#' \item{fp}{The double value of false postives. Actual = 0, predicted = 1.}
+#' \item{fn}{The double value of false negatives. Actual = 1, predicted = 0.}
+#' \item{prev}{The double value of prevalence.}
+#' \item{acc}{The double value of accuracy.}
+#' \item{sens}{The double value of sensitivity.}
+#' \item{spec}{The double value of specificity.}
+#' \item{fdr}{The double value of the false discovery rate.}
+#' \item{dor}{The double value of the diagnostic odds ratio.}
+#' }
+#' @author Cole Kenney
+#' @export
 confusion_matrix_and_metrics <- function(predictor, response, cutoff) {
   optim_beta <- beta_hat(predictor, response)
   p <- 1 / (1 + exp(-predictor %*% optim_beta))
@@ -33,7 +57,16 @@ confusion_matrix_and_metrics <- function(predictor, response, cutoff) {
   fdr = fp/p_predictions
   dor = (sensitivity/(1-specificity))/((1-sensitivity)/specificity)
   
-  return(list("Prevalence" = prevalence, "Accuracy" = accuracy, "Sensitivity" = sensitiviy, "Specificity" = specificity, "False Discovery Rate" = fdr, "Diagnostic Odds Ratio" = dor))
+  return(list("tp" = tp,
+              "tn" = tn,
+              "fp" = fp,
+              "fn" = fn,
+              "prev" = prevalence, 
+              "acc" = accuracy, 
+              "sens" = sensitiviy, 
+              "spec" = specificity, 
+              "fdr" = fdr, 
+              "dor" = dor))
 }
 
 #plotting metrics starter function - with prevalence as example
@@ -42,8 +75,7 @@ plot_metrics_starter_function <- function(predictor, response) {
   colnames(plot_df) <- c("Cut_Off", "Prevalence")
   
   for(i in seq(from = 0.1, to = 0.9, by = 0.1)) {
-    plot_df[nrows(plot_df)+1, ] <- c(i, confusion_matrix_and_metrics(predictor, response, i)$Prevalence)
+    plot_df[nrows(plot_df)+1, ] <- c(i, confusion_matrix_and_metrics(predictor, response, i)$prev)
     }
   #this will give you a data frame with the specified metric for each cutoff value. plot data frame from here / repeat for all metrics listed
 }
-
